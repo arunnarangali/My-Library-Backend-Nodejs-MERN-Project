f912baf06da874d4ed29c8b99bcb0ae53d03bf61
arunnarangali <arunnarangali123@gmail.com>
2024-04-13 15:05:20 +0530

    refactor:add .env file

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const connectDB = require('./config/db');
-const testRouter = require('./Router/testRouter');
-const dotenv = require('dotenv');
-const cors = require('cors');
+const express = require("express");
+const connectDB = require("./config/db");
+const testRouter = require("./Router/testRouter");
+const dotenv = require("dotenv");
+const cors = require("cors");
 
 // Load environment variables from .env file
 dotenv.config();
@@ -14,33 +14,34 @@ connectDB();
 const app = express();
 
 // Enable CORS for all routes
-app.use(cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"]
-}));
+const corsConfig = {
+  origin: `${process.env.API_URL}`,
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+};
+app.use(cors(corsConfig));
 
 // Body parsing middleware
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 // Routes
-app.use('/', testRouter);
+app.use("/", testRouter);
 
 // Default route
-app.get('/', (req, res) => {
-    res.json("Server is running");
-    console.log("Server is running");
+app.get("/", (req, res) => {
+  res.json("Server is running");
+  console.log("Server is running");
 });
 
 // Error handling middleware
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
 });
 
 // Port configuration
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
